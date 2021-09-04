74136e0223f5074e2c6aae0295498cfbff7d9f7e
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2021-09-04 19:21:10 +0200

    Vídeo 09

---
+++ D-DGS-Node/src/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    id: Number | undefined,
+    nickname: String | undefined,
+    email: String | undefined,
+    password: String | undefined,
+    is_active: Boolean | undefined,
+    type_user: Boolean | undefined,
+});
+
+const User = mongoose.model('User', userSchema);ç
+
+module.exports = User;
