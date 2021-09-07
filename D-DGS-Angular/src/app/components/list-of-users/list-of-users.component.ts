54dad1789e96d26aee01c8552fee5e695761e0bd
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2021-09-07 20:01:42 +0200

    Ahora leemos los usarios de la BD en la ruta /

--- D-DGS-Angular/src/app/components/list-of-users/list-of-users.component.ts
+++ D-DGS-Angular/src/app/components/list-of-users/list-of-users.component.ts
@@ -8,36 +8,13 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class ListOfUsersComponent implements OnInit {
 
-  public hardcodedUsers: any = [{
-    id: 1,
-    nickname: 'pacoTabaco',
-    email: 'pacotabaco@gmail.com',
-    password: '1234',
-    is_active: true,
-    type_user: true
-  },
-  {
-    id: 2,
-    nickname: 'jonnyMelavo',
-    email: 'jonnymelavo@gmail.com',
-    password: '1234',
-    is_active: true,
-    type_user: true
-  },
-  {
-    id: 3,
-    nickname: 'elenaNito',
-    email: 'elenanito@gmail.com',
-    password: '1234',
-    is_active: true,
-    type_user: true
-  },]
+  public users: any = [];
 
   constructor(private _userService: UsersService) { }
 
   ngOnInit(): void {
     this._userService.getUsers().subscribe(users => {
-      console.log('List of users', users);
+      this.users = users;
     })
   }
 
