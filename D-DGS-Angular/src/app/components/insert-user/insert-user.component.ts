88c34f461d137dd389701556ebc99a7dc1f2a895
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2021-09-30 11:20:27 +0200

    Todo listo para librería de diagramas

--- D-DGS-Angular/src/app/components/insert-user/insert-user.component.ts
+++ D-DGS-Angular/src/app/components/insert-user/insert-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-insert-user',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertUserComponent implements OnInit {
 
-  constructor() { }
+  public newUser: User = new User();
+
+  constructor(private _usersService: UsersService,
+              private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  insertUserInDB() {
+    console.log("The user which will bew inserted into db is", this.newUser)
+    this._usersService.postUser(this.newUser).subscribe(user => {
+      if(user) {
+        this._router.navigateByUrl('/users');
+      }
+    })
   }
 
 }
