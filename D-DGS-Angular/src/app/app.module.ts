063335ed6dd9599383ca5190a4c9cb78bb4d3731
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2021-11-22 11:20:33 +0100

    Reinicio

---
+++ D-DGS-Angular/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http"; // Connection via HTTP protocol with node API
+import { RouterModule, Routes } from "@angular/router"; // Routing through the website
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListOfUsersComponent } from './components/list-of-users/list-of-users.component';
+import { InsertUserComponent } from './components/insert-user/insert-user.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UserComponent } from './components/user/user.component';
+import { TestDiagramComponent } from './components/test-diagram/test-diagram.component';
+
+const routes: Routes = [
+  {path: 'users', component: ListOfUsersComponent},
+  {path: 'users/:id', component: UserComponent},
+  {path: 'insert_user', component: InsertUserComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+  {path: 'test', component: TestDiagramComponent},
+  {path: '**', component: PageNotFoundComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListOfUsersComponent,
+    InsertUserComponent,
+    PageNotFoundComponent,
+    UserComponent,
+    TestDiagramComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
