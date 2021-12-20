17c6978726ea734bfb28551b8d427f83a4e20380
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2021-12-20 17:31:54 +0100

    No funcionan los métodos para acceder a la BD.

--- D-DGS-Angular/src/app/components/diagram-creation/diagram-creation.component.ts
+++ D-DGS-Angular/src/app/components/diagram-creation/diagram-creation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 import { Activity } from 'src/app/models/activity';
+import { Task } from 'src/app/models/task';
 import { DiagramDomainService } from "src/app/services/diagramDomain.service";
 
 @Component({
@@ -10,12 +12,23 @@ import { DiagramDomainService } from "src/app/services/diagramDomain.service";
 export class DiagramCreationComponent implements OnInit {
   
   public Activities: any = [];
+  public newTasksArray: Task[] = new Array<Task>();
+  public activityToCreate: Activity = new Activity("","",this.newTasksArray);
+  public parentActivityForTheNewTask: Activity = new Activity("","",this.newTasksArray);
+  public taskToCreate: Task = new Task("","", this.parentActivityForTheNewTask);
+
+  // Flags
+  public ActivitiesOnDB: boolean = false;
   public userInDomainFase: boolean = true;
   public userInRewardFase: boolean = false;
   public userInDesingFase: boolean = false;
-  public ActivitiesOnDB: boolean = false;
+  public addNewActivityIsClicked: boolean = false;
+  public addNewTaskIsClicked: boolean = false;
 
-  constructor(private _diagramDomainService: DiagramDomainService) { }
+  
+
+  constructor(private _diagramDomainService: DiagramDomainService,
+              private _router: Router) { }
 
   ngOnInit(): void {
     this._diagramDomainService.getActivities().subscribe(activities => {
@@ -45,6 +58,40 @@ export class DiagramCreationComponent implements OnInit {
     this.userInDomainFase = false;
     this.userInRewardFase = false;
     this.userInDesingFase = true;
+  }
+
+  addNewActivityClicked()
+  {
+    this.addNewActivityIsClicked = (this.addNewActivityIsClicked) ? false: true;
+  }
+
+  addNewTaskClicked(parentActivity: Activity)
+  {
+    this.parentActivityForTheNewTask = parentActivity;
+    this.addNewTaskIsClicked = (this.addNewTaskIsClicked) ? false: true;
+  }
+
+  activtyHasTaskAttached(activityToEvaluate: Activity) // To display or not some text on the page view
+  {
+    return activityToEvaluate.tasks.length != 0;
+  }
+
+  addNewActivity()
+  {
+    this._diagramDomainService.postANewActivity(this.activityToCreate).subscribe(activity => {
+      if(activity)
+        this._router.navigateByUrl('/create_a_diagram');
+    });
+  }
+
+  addNewTaskToAnActivity()
+  {
+    this.taskToCreate.activity = this.parentActivityForTheNewTask; //Updating parent activity
+    this._diagramDomainService.postANewTask(this.taskToCreate);
+    
+    this.parentActivityForTheNewTask.tasks.push(this.taskToCreate);
+    this._diagramDomainService.updateAnActivity(this.parentActivityForTheNewTask);
+
   }
 
 }
