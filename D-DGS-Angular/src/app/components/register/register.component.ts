cbe1e3df7ccf4c9787f28984e801296fe906825b
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2022-05-23 11:32:53 +0200

    Default Set y Context se crean al crear al usuario.

--- D-DGS-Angular/src/app/components/register/register.component.ts
+++ D-DGS-Angular/src/app/components/register/register.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Context } from 'src/app/models/context';
+import { Reward_Set } from 'src/app/models/reward_set';
 import { User } from 'src/app/models/user';
+import { DiagramDomainService } from 'src/app/services/diagramDomain.service';
 import { UsersService } from 'src/app/services/users.service';
 
+const DEFAULT_CONTEXT_NAME: string = "Default Context";
+const DEFAULT_SET_NAME: string = "Default Reward Set";
 const LOG_TOKEN: string = "LOG_TOKEN";
 // LOG_TOKEN: null | FAILED | identificador del dominio del usuario
 // * DEBE SUSTITUIRSE EN ALGÚN MOMENTO POR ALGO CIFRADO
@@ -14,7 +19,7 @@ const LOG_TOKEN: string = "LOG_TOKEN";
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private _router: Router, private _userService: UsersService) { }
+  constructor(private _router: Router, private _userService: UsersService, private _diagramDomainService: DiagramDomainService) { }
 
   public userToCreate: User = new User("","","","",false);
 
@@ -32,7 +37,11 @@ export class RegisterComponent implements OnInit {
     this.userToCreate.domainIdentificator = this.userToCreate.nickname+"IDENTIFICATOR";
         
     this._userService.postUser(this.userToCreate).subscribe((state: any) => {
-      this._router.navigateByUrl('/login');
+      this._diagramDomainService.postANewContext(new Context(DEFAULT_CONTEXT_NAME, this.userToCreate.domainIdentificator)).subscribe(res => {
+        this._diagramDomainService.postANewRewardSet(new Reward_Set(DEFAULT_SET_NAME, this.userToCreate.domainIdentificator)).subscribe(res => {
+          this._router.navigateByUrl('/login');
+        })
+      })
     })
   }
 
