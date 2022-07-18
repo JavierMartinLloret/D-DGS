8b8a1b7336890e538cfb906ea6bf89c501492f21
JavierMartinLloret <javier.martinlloret@alum.uca.es>
2022-07-18 12:05:04 +0200

    Aregladas cositas de compatibilidad SO y registro en lo visual.

---
+++ scriptLauncher.py
@@ -0,0 +1,4 @@
+import os
+os.system('cmd /k ""C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" -dbpath "C:\data\db""')
+
+# "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" -dbpath "C:\data\db"
